cd54764e371bf3a05fde52ee5d9a2a565a8d1d00
Arthur <nuntapopsangehot@gmail.com>
2023-03-09 23:30:25 +0700

    Update saveorder.php

--- saveorder.php
+++ saveorder.php
@@ -9,22 +9,32 @@ $total = $_POST['total'];
 
 
 
-$quey1 = "INSERT INTO order_head ( `customer_id`, `date`, `total`, `address`, `phone`, `email`) VALUES ( '$customer_id', '$date', '$total', '$address', '$phone', '$email')";
+$query1 = "INSERT INTO order_head ( `customer_id`, `date`, `total`, `address`, `phone`, `email`) VALUES ( '$customer_id', '$date', '$total', '$address', '$phone', '$email')";
 $result = mysqli_query($connect, $query1);
 
-$quey2 ="SELECT max(id) FROM order_head where customer_id = $customer_id and email = $email and phone =$phone";
+$query2 ="SELECT max(id) as id FROM order_head where customer_id = $customer_id  ";
 $result2 = mysqli_query($connect, $query2);
-$row = mysqli_fetch_assoc($result2);
+$row = mysqli_fetch_array($result2);
 $order_id = $row["id"];
 //not edit yet
 foreach($_SESSION['cart'] as $p_id=>$qty)
 {
-    $sql3	= "select * from product where p_id=$p_id";
-    $query3	= mysqli_query($conn, $sql3);
-    $row3	= mysqli_fetch_array($query3);
-    $total	= $row3['p_price']*$qty;
+    $sql3	= "SELECT * from product where id=$p_id";
+    $query3	= mysqli_query($connect, $sql3);
+    $row3	= mysqli_fetch_assoc($query3);
+    $total	= $row3['product_price']*$qty;
     
-    $sql4	= "insert into order_detail values(null, '$o_id', '$p_id', '$qty', '$total')";
-    $query4	= mysqli_query($conn, $sql4);
+    $sql4	= "INSERT INTO order_detail ( `order_head_id`, `product_id`, `qty`, `sub_total`) 
+    VALUES  ( '$order_id', '$p_id', '$qty', '$total')";
+    $query4	= mysqli_query($connect, $sql4);
 }
+
+if($query1 and $query4 ){
+    echo "trasition success";
+    foreach($_SESSION['cart'] as $id){
+        unset($_SESSION['cart']);
+    }
+    Header("Location:shop.php");
+}
+
 ?>
