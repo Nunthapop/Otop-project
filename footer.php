ace96444e31349b12aa8581c995cf05277ca9091
Patitta16 <Patitta.162547@gmail.com>
2023-03-11 18:55:23 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <html> 
     <link rel="stylesheet" href="ft.css">
     <body> 
-        <div class="footer" style="height: 70px">
+        <div class="footer1" style="height: 70px">
 Copyright 2023 connoisseur, Lampang City
 
 </div> 
