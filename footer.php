6665a266291f617cc0b3123827ed8ef975425aef
Patitta16 <Patitta.162547@gmail.com>
2023-03-11 18:43:52 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<html> 
+    <link rel="stylesheet" href="ft.css">
+    <body> 
+        <div class="footer" style="height: 70px">
+Copyright 2023 connoisseur, Lampang City
+
+</div> 
+</body>
+ </html>
